7922686fcb506bea6160461fffca4cfda9557d52
Nathan Shull <nshull16@iastate.edu>
2019-03-26 17:42:37 -0500

    Setting up node class

--- pa1/src/pa1/Node.java
+++ pa1/src/pa1/Node.java
@@ -4,9 +4,98 @@ public class Node {
 	private int color;
 	private int value;
 	private int maxvalue;
+	private Endpoint endpoint;
+	private Endpoint max;
 	private Node parent;
 	private Node left;
 	private Node right;
-	private Endpoint endpoint;
-	private Endpoint max;
+	
+	public Node(){
+		color = 0; //0 represents red, 1 represents black
+		val = 0;
+		maxvalue = 0;
+		endpoint = new Endpoint(0,0);
+		max = new Endpoint(0,0);
+		parent = null;
+		left = null;
+		right = null;
+	}
+	
+	public Node(Endpoint endpoint){
+		color = 0;
+		val = 0;
+		maxvalue = 0;
+		this.endpoint = endpoint;
+		max = endpoint;
+		parent = null;
+		left = null;
+		right = null;
+	}
+	
+	public int getColor(){
+		return color;
+	}
+	
+	public int setColor(int color){
+		this.color = color;
+	}
+	
+	public int getVal()
+	{
+		return val;
+	}
+	
+	public int setVal(int val){
+		this.val = val;
+	}
+	
+	public int getMaxValue(){
+		return maxvalue;
+	}
+	
+	public int setMaxValue(){
+		this.maxvalue = maxvalue;
+	}
+	
+	public Endpoint getEndpoint(){
+		return endpoint;
+	}
+	
+	public void setEndpoint(Endpoint endpoint){
+		this.endpoint = endpoint;
+	}
+	
+	public Endpoint getMax(){
+		return max;
+	}
+	
+	public void setMax(Endpoint max){
+		this.max = max;
+	}
+	
+	public Node getParent(){
+		return parent;
+	}
+	
+	public void setParent(Node parent){
+		this.parent = parent;
+	}
+	
+	public Node getLeft(){
+		return leftt;
+	}
+	
+	public void setLeft(Node left){
+		this.left = left;
+	}
+	
+	public Node gerRight(){
+		return right;
+	}
+	
+	public void setRight(Node right){
+		this.right = right;
+	}
+	
+	
 }
