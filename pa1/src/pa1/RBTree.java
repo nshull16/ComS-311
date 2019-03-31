ca050eb864dd1878d6a4a6831808b113a7622321
Nathan Shull <nshull16@iastate.edu>
2019-03-31 12:46:56 -0500

    Insert and insert fixup

--- pa1/src/pa1/RBTree.java
+++ pa1/src/pa1/RBTree.java
@@ -40,6 +40,84 @@ public class RBTree {
 		this.root = node;
 	}
 	
+	public void InsertNode(Node newNode){
+		Node y = new Node();
+		y = this.getNILNode();
+		Node x = new Node();
+		x = this.getRoot();
+		this.setSize(this.getSize()+1);
+		
+		while(x != this.getNILNode){
+			y = x;
+			if(newNode.getKey() < x.getKey()){
+				x = x.getLeft();
+			}
+			else{
+				x = x.getRight();
+			}
+		}
+		newNode.setParent(y);
+		if(y == this.getNILNode()){
+			this.setRoot(newNode);
+			newNode.setLeft(this.getNILNode());
+			newNode.setRight(this.getNILNode());
+			newNode.setColor(1);
+			return;
+		}
+		else if (newNode.getKey() < newNode.getKey()){
+			y.setLeft(newNode);
+		}
+		else{
+			y.setRight(newNode);
+		}
+		newNode.setLeft(this.getNILNode());
+		newNode.setRight(this.getNILNode());
+		InsertNodeFixup(newNode);
+	}
+	
+	public void InsertNodeFixup(Node newNode){
+		while(newNode.getParent().getColor() == 0){
+			if(newNode.getParent() == newNode.getParent().getParent().getLeft()){
+				Node y = new Node();
+				y = newNode.getParent().getParent().getRight();
+				if(y.getColor() == 0){
+					newNode.getParent().setColor(1);
+					y.setColor(1);
+					newNode.getParent.getParent.setColor(0);
+					newNode = newNode.getParent().getParent();
+				}
+				else{
+					if(newNode == newNode.getParent.getRight()){
+						newNode = newNode.getParent();
+						LeftRotate(newNode);
+					}
+					newNode.getParent().setColor(1);
+					newNode.getParent().getParent().setColor(0);
+					RightRotate(newNode.getParent().getParent());
+				}
+			}
+			else{
+				Node y = new Node();
+				y = newNode.getParent().getParent().getLeft();
+				if(y.getColor() == 0){
+					newNode.getParent().setColor(1);
+					y.setColor(1);
+					newNode.getParent().getParent().setColor(0);
+					newNode = newNode.getParent().getParent();
+				}
+				else{
+					if(newNode == newNode.getParent().getLeft()){
+						newNode = newNode.getParent();
+						RightRotate(newNode);
+					}
+					newNode.getParent().setColor(1);
+					newNode.getParent().getParent().setColor(0);
+					LeftRotate(newNode.getParent().getParent());
+				}
+			}
+		}
+		this.getRoot().setColor(1);
+	}
 	public void LeftRotate(Node x){
 		Node y = new Node();
 		y = x.getRight();
